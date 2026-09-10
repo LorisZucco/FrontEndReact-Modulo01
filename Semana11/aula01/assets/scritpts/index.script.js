16158b9c8a0c146ec881dcc43701314ccfc52076
LorisZucco <lorisjunior@gmail.com>
2026-09-10 19:10:25 -0300

    feat: adiciona api de consumo de terceiros via fetch

---
+++ Semana11/aula01/assets/scritpts/index.script.js
@@ -0,0 +1,53 @@
+console.log("Javascript Avançado!");
+
+const uf = document.getElementById("uf");
+
+async function searchAddress(cep) {
+  const apiURL = "https://viacep.com.br/ws/" + cep + "/json/";
+
+  const response = await fetch(apiURL);
+  const data = await response.json();
+
+  if (data.erro != undefined) {
+    alert("CEP não encontrado!");
+    return;
+  }
+
+  console.log(data.logradouro + ", " + data.uf);
+  uf.textContent = uf.textContent + " " + data.uf;
+}
+
+searchAddress("13271450");
+
+async function harryPotterCharacters(index) {
+  const photo = document.getElementById("photo");
+  const apiURL = "https://hp-api.onrender.com/api/characters";
+  const response = await fetch(apiURL);
+  const data = await response.json();
+  console.log(data);
+  console.log(data[index].alternate_names[1]);
+
+  photo.innerHTML = `<img src = ${data[index].image}>`;
+}
+harryPotterCharacters(2);
+
+async function rate() {
+  try {
+    const apiURL = "https://economia.awesomeapi.com.br/last/USD-BRL";
+
+    const response = await fetch(apiURL);
+    // console.log(response.ok);
+
+    if (response.ok == false) {
+      alert("Requisiçao falha!");
+      return;
+    }
+
+    const data = await response.json();
+
+    console.log(data.USDBRL);
+  } catch (error) {
+    console.log("Erro ao converter!");
+  }
+}
+rate();
